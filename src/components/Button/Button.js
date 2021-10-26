232bde5c754d561a52784ad378f41d59c3e27c2e
jeylaa <djeyla96@gmail.com>
2021-10-26 23:21:14 +0300

    add css

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,3 +1,4 @@
+import "./Button.css";
 const { useState } = require("react");
 
 function HideText() {
@@ -5,7 +6,14 @@ function HideText() {
 
   return (
     <div className="App">
-      {hideText && <h1>This text will show!</h1>}
+      {hideText && (
+        <h3>
+          You wouldn't plant a seed and then dig it up every few minutes to see
+          if it has grown. So why do you keep questioning yourself, your hard
+          work and your decisions? Have patience, stop overthinking and keep
+          watering your seeds.
+        </h3>
+      )}
       <button onClick={() => setHideText(!hideText)}>Toggle Text</button>
     </div>
   );
